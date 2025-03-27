6ecba4c920be7513f85e2a663995fa8b7cdd11f4
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2025-03-27 15:19:38 -0300

    Update funcoes.js

--- Frameworks/1Bimestre/aula(20-03-25)/funcoes.js
+++ Frameworks/1Bimestre/aula(20-03-25)/funcoes.js
@@ -3,45 +3,64 @@ var ctxLinks;
 var ctxConteudo;
 var ctxRodape;
 
-function configEstiloCabecalho(){
-    bg=document.getElementById("corFundo").value;
-    corFonte=document.getElementById("corFonte").value;
-    tamFonte=document.getElementById("tamFonte").value;
-    ctxCabecalho="#cabecalho{\n background-color:"+bg+";\n";
-    ctxCabecalho+=" color: "+corFonte+":\n";
-    ctxCabecalho+=" font-size: "+tamFonte+"pt;\n}\n";
+function configEstiloCabecalho() {
+    bg = document.getElementById("corFundo").value;
+    corFonte = document.getElementById("corFonte").value;
+    tamFonte = document.getElementById("tamFonte").value;
+    ctxCabecalho = "#cabecalho{\n background-color:" + bg + ";\n";
+    ctxCabecalho += " color:" + corFonte + ";\n";
+    ctxCabecalho += " font-size:" + tamFonte + "pt;\n}\n";
     return ctxCabecalho;
 }
-function configEstiloLinks(){
-    corLink=document.getElementById("corLinks").value;
-    estiloLinks=document.querySelector('input[name="estiloLinks"]:checked').value;
-    ctxLinks="a{\n color:"+corLink+":\n";
-    let aux=estiloLinks=="0"?"none":"underline";
-    ctxLinks+=" text-decoration:"+aux+";\n}\n";
+function configHtmlLinks() {
+    links = document.querySelector("#links").value;
+    vetLinks = links.split(";");
+    ctxLinks = "";
+    for (let index = 0; index < vetLinks.length; index++) {
+        ctxLinks += '<a href="#">' + vetLinks[index] + '</a>\n';
+    }
     return ctxLinks;
 }
-function configHtmlLinks(){}
-function configHTMLCabecalho(){
+function configHTMLCabecalho() {
     let aux = document.querySelector("#textoCabecalho").value;
-    ctxCabecalho='<h1>'+aux+'</h1>';
+    ctxCabecalho = '<h1>' + aux + '</h1>';
     return ctxCabecalho;
 }
-
-function gerarCodigo(){
-    //Código para CSS
-    let codeCss=document.querySelector("#codeCSS");
-    let css=configEstiloCabecalho();
-    css+=configEstiloLinks();
-    codeCss.value=css;
-    //Código para Html
-    let codeHTML=document.querySelector("#codeHTML");
-    ctxHTML="<html>\n<head>\n" +
-    "<link rel='stylesheet' href='estilo.css'>\n" +
-    "<title>Minha página</title>\n" + "</head>\n<body>" +
-    "<div> id='cabecalho'>" + configHTMLCabecalho()+"\n</div>\n" +
-    "<nav id='links'>\n" + configHtmlLinks() + "\n</nav>\n" +
-    "</body>\n</html>";
-    codeHTML.value=ctxHTML;
+function configEstiloLinks() {
+    corLink = document.getElementById("corLinks").value;
+    estiloLinks = document.querySelector('input[name="estiloLinks"]:checked').value;
+    ctxLinks = "a{\n color:" + corLink + ";\n";
+    let aux = estiloLinks == "0" ? "none" : "underline";
+    ctxLinks += " text-decoration:" + aux + ";\n}\n";
+    return ctxLinks;
 }
 
-function download(campo, arquivo){}
+function gerarCodigo() {
+    //Cóigo para CSS
+    let codeCSS = document.querySelector("#codeCSS");
+    let css = configEstiloCabecalho();
+    css += configEstiloLinks();
+    codeCSS.value = css;
+    //Código para HTML
+    let codeHTML = document.querySelector("#codeHTML");
+    ctxHTML = "<html>\n<head>\n" +
+        "<link rel='stylesheet' href='estilo.css'>\n" +
+        "<title>Miha página</title>\n" +
+        "</head>\n<body>" +
+        "<div id='cabecalho'>" + configHTMLCabecalho() + "</div>\n" +
+        "<nav id='links'>\n" + configHtmlLinks() + "\n</nav>\n" +
+        "<div id='conteudo'></div>\n" +
+        "</body>\n</html>";
+    codeHTML.value = ctxHTML;
+
+}
+function download(campo, arquivo) {
+    var text = document.getElementById(campo).value;
+    var blob = new Blob([text], { type: "text/plain" });
+    var a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = arquivo;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
