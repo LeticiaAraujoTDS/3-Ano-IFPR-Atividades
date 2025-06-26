7119695c2a1bd0ac11d8c2c449e548b46273d60b
Leticia <134729304+LeticiaAraujoTDS@users.noreply.github.com>
2025-06-26 15:42:46 -0300

    Add files via upload

---
+++ Frameworks/2Bimestre/aula(26-06-25)/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário de Conexão</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<div class="container">
+
+<form action="creator.php" method="POST">
+    <?php
+
+    include 'mensagens.php';
+    if (isset($_GET['msg']) ){
+        $msg = $_GET["msg"];
+        $classe=$msg==2?'mensagem':'mensagem_erro';
+        echo "<div class=\"$classe\">" . ($mensagens[$msg] ?? "Erro desconhecido") . "</div>";
+    }
+    ?>
+    <h1>EasyMVC</h1><h2>Configuração</h2>
+
+    <label for="servidor">Servidor:</label>
+    <input type="text" id="servidor" name="servidor" required>
+
+    <label for="banco">Banco de Dados:</label>
+    <input type="text" id="banco" name="banco" required>
+    <!-- <select name="banco" id="banco">Banco de Dados: </select>
+    <option value="0">---</option>
+     usar o buscaTabelas() para criar as options com os bancos já existentes, para evitar erros de digitação-->
+
+    <label for="usuario">Usuário:</label>
+    <input type="text" id="usuario" name="usuario" required>
+
+    <label for="senha">Senha:</label>
+    <input type="password" id="senha" name="senha">
+
+    <button type="submit">Enviar</button>
+</form>
+</div>
+</body>
+</html>
